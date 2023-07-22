0fb4cfa41ce55f8b29bc3b64fdd4c8a437969d1e
wonu606 <wonu606@gmail.com>
2023-07-22 21:12:51 +0900

    init: 프로젝트 생성
    - springboot 3버전 프로젝트 생성
    - .gitignore 초기화

---
+++ managing-products/src/main/java/com/wonu606/managingproducts/ManagingProductsApplication.java
@@ -0,0 +1,13 @@
+package com.wonu606.managingproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagingProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagingProductsApplication.class, args);
+	}
+
+}
---
+++ managing-products/src/test/java/com/wonu606/managingproducts/ManagingProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wonu606.managingproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagingProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
